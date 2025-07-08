a786147f16df5deca1d6e09eea72bc7717980998
IrinaM71 <git@github.com>
2025-07-08 17:08:05 +0200

    Скопировала иконки и логотипы

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import finance from "../../assets/icons/finance.svg";
+function Home() {
+  return (
+    <>
+      <h1>Home page</h1>
+      <img src="{financeIcon}" alt="logo" />
+    </>
+  );
+}
+
+export default Home;
